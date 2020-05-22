c764c39aea63a1777bc8cde81ea40c8ec6f6352b
moe <moe@busyloop.net>
2020-05-22 13:43:00 +0200

    Fix expires_date

--- lib/monza/verification_response.rb
+++ lib/monza/verification_response.rb
@@ -39,7 +39,7 @@ module Monza
 
     def latest_expiry_date
       # @latest_receipt_info.last.expires_date_ms if @latest_receipt_info.last
-      @latest_receipt_info.collect(&:expires_date).max if @latest_receipt_info.last
+      @latest_receipt_info.collect(&:expires_date_ms).max if @latest_receipt_info.last
     end
 
     class VerificationError < StandardError
